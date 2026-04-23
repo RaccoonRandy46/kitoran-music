afc25c4cb17f8463d7ddb40f5196029f5417a584
RaccoonRandy46 <rediscoop@gmail.com>
2026-04-23 15:10:35 -0700

    fixed return button

--- components/back-btn.js
+++ components/back-btn.js
@@ -1,4 +1,24 @@
 document.querySelector('.back-btn').addEventListener('click', function(event) {
+    
     event.preventDefault();
-    window.history.back();
+
+    if (window.history.length > 1) {
+
+        const referrer = document.referrer;
+
+        if (referrer) {
+            try {
+                const referrerUrl = new URL(referrer);
+
+                if (referrerUrl.origin === window.location.origin) {
+                    window.history.back();
+                    return;
+                }
+            } catch (e) {}
+        }
+
+    }
+
+    window.location.href = 'index.html';
+
 });
